d653a58c07f9853e49998eb98a7650fba0f2bf87
forgere <forgere123zw@gmail.com>
2019-12-26 13:55:37 +0800

    update test

--- test/test.js
+++ test/test.js
@@ -7,8 +7,18 @@ const cp = require('child_process')
 const your_donwload_path = path.join(__dirname, "./down")
 
 describe('Functions', function () {
-    describe('#getmusic', function () {
-        this.timeout(5000);
+    this.beforeAll(() => {
+        if (!fs.existsSync(your_donwload_path)) {
+            fs.mkdirSync(your_donwload_path)
+        }
+    })
+    this.timeout(5000);
+    this.afterAll(()=>{
+        const files = path.join(__dirname, "./down")
+
+        cp.exec(`rm -rf ${files}`, () => {})
+    })
+    describe('#download', function () {
         it('我曾.mp3 should exist file', function (done) {
             getmusic.download('我曾', your_donwload_path).then(() => {
                 if (!fs.existsSync(path.join(__dirname, '/down/我曾.mp3'))) {
@@ -17,7 +27,9 @@ describe('Functions', function () {
                 done()
             }).catch(err => done(err))
         });
+    });
 
+    describe('#downloadFilter', function () {
         it('多想在平庸的生活拥抱你.mp3 should exist file', function (done) {
             getmusic.downloadFilter('多想在平庸的生活拥抱你', your_donwload_path, "隔壁老樊").then(() => {
                 if (!fs.existsSync(path.join(__dirname, '/down/多想在平庸的生活拥抱你.mp3'))) {
@@ -26,7 +38,10 @@ describe('Functions', function () {
                 done()
             }).catch((err) => done(err))
         });
+    });
 
+
+    describe('#downloadLyric', function () {
         it('should return string', function (done) {
             getmusic.downloadLyric('稻香', '周杰伦').then((lyric) => {
                 //do something
@@ -35,8 +50,3 @@ describe('Functions', function () {
         });
     });
 });
-
-
-const files = path.join(__dirname, "./down/*")
-
-cp.exec(`rm -rf ${files}`, () => {})
